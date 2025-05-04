730581f71e75212186810bd922de58791d1589d2
Gleason-o7 <sethcgleason@gmail.com>
2025-05-04 13:52:09 -0700

    completed lesson 2 chapter 10.3 program

---
+++ 2.10.3.py
@@ -0,0 +1,34 @@
+#2d table
+# ask for numrows, numcolumns, gen table, get row/column index and output va
+
+#Get user input defining table bounds
+num_rows = int(input("Enter the number of rows: "))
+num_columns = int(input("Enter the number of coluns: "))
+
+# generate table based on size
+# note: table will be filled, so values need not be initialized
+
+
+# Original for loop run:
+'''
+accumulator = 0
+for i in range(num_columns):
+    for j in range(num_rows):
+        table[i][j] = accumulator
+        accumulator += 1
+'''
+
+# cooler for loop initializer
+table = [[(i * num_columns + j) for j in range(num_columns)] for i in range(num_rows)]
+
+user_idx = int(input("Enter index: "))
+
+row_val = user_idx // num_columns
+column_val = user_idx % num_columns
+
+
+print(f"Row #{row_val}")
+print(f"Column #{column_val}")
+
+print(f"({row_val},{column_val})")
+
